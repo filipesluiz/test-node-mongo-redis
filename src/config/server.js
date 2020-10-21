71f0bed733087215b374a13b2933fac81b4bfb32
filipesluiz <filipesluiz@gmail.com>
2020-10-21 10:25:50 -0300

    add log for test

--- src/config/server.js
+++ src/config/server.js
@@ -1,6 +1,7 @@
 const express = require('express'); // () execute function express module return
 const bodyParser = require('body-parser');
-const userRoute = require('../routes/userRoute'); 
+//const userRoute = require('../routes/userRoute');  
+
 const app = express();
 
 require('../routes/index')(app); //config routes in index by ref
@@ -9,10 +10,11 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-app.use('/service', userRoute);
+//app.use('/service', userRoute);
 app.set('view engine', 'ejs');
 app.set('views', './src/views'); //the path starts from where this module will be used, in app.js. 
 
-const redis = require('redis');
-const redisClient = null;// = redis.createClient();
-module.exports = { app, redisClient };
+//const redis = require('redis');
+//const redisClient = null;// = redis.createClient();
+//module.exports = { app, redisClient };
+module.exports = { app };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 //const ArtigosRepository = require('../repositories/artigosRepository');
+var logger = require('../config/logger');
 
 module.exports = (app) => {
 
     app.get('/', (req, res) => {
+       logger.info('Endpoint home!');
        res.render('home/index');
        /* ArtigosRepository.findAll((result) => {
             console.log('Query Route', result);
@@ -11,10 +13,14 @@ module.exports = (app) => {
     });
     
     app.get('/add_noticia', (req, res) => {
+        logger.info('Endpoint Add!');
         res.render("admin/form_add_noticia");
     });
     
-    app.get('/noticias', (req, res) => res.render("noticias/noticias"));
+    app.get('/noticias', (req, res) => {
+        logger.info('Endpoint noticias!');
+        res.render("noticias/noticias");
+    });
 
 }    
 
